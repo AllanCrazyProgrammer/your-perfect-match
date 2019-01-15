d8f294ff8a641075c0c8ee6d812796273bc87b99
Allan <32887387+AllanCrazyProgrammer@users.noreply.github.com>
2019-01-14 23:56:29 -0600

    working in progress (matching logic)

--- app/public/server.js
+++ app/public/server.js
@@ -7,7 +7,41 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var person = [];
+var person = [
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+
+    {
+        "name": "nadia",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            "1",
+            "2",
+            "5",
+            "2",
+            "3",
+            "2",
+            "5",
+            "2",
+            "5",
+            "5"
+        ]
+    }
+];
 
 
 app.get('/', function (req, res) {
@@ -23,12 +57,15 @@ app.get("/api/friends", function (req, res) {
 
 //Recibe datos del survey y los agrega al ojeto de person.
 app.post("/api/friends", function (req, res) {
-
+    
     let newPerson = req.body;
-
-    console.log(newPerson);
-
+    let scores = newPerson.scores;
     person.push(newPerson);
+
+
+    for (let i = 0; i < person.length; i++) {
+        console.log(person[i].scores);
+    }
 
     res.json(newPerson);
 });
