d1662b233315e9d49bc7450ab0d4b8a9d40b607c
Allan <32887387+AllanCrazyProgrammer@users.noreply.github.com>
2019-01-19 14:59:15 -0600

    changed routes

--- server.js
+++ server.js
@@ -45,10 +45,10 @@ var person = [
 
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
+    res.sendFile(path.join(__dirname, "./app/public/home.html"));
 })
 app.get('/survey', function (req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
+    res.sendFile(path.join(__dirname, "./app/public/survey.html"));
 })
 
 app.get("/api/friends", function (req, res) {
